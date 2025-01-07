c3ea9507ea372e77f1e12c15e409061e2b9e676e
mauro <mauro_leao@hotmail.com>
2025-01-07 19:13:35 -0300

    Finalizado o crud inicial, agora falta adicionar mais campos e trabalhar no frontend

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
-    idade = models.IntegerField()
+    idade = models.IntegerField(default=0)  # Valor padrão adicionado
     
     def __str__(self):
         return self.nome
--- core/views.py
+++ core/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Pessoa
 
 def home(request):
-    return render(request, "index.html")
+    pessoas = Pessoa.objects.all()
+    return render(request, 'index.html', {'pessoas': pessoas})
 
+def salvar(request):
+    if request.method == 'POST':
+        nome = request.POST.get('nome', '')
+        idade = request.POST.get('idade', 0)
+        Pessoa.objects.create(nome=nome, idade=idade)
+    return redirect('home')
+
+def editar(request, id):
+    pessoa = get_object_or_404(Pessoa, id=id)
+    return render(request, 'update.html', {'pessoa': pessoa})
+
+def update(request, id):
+    if request.method == 'POST':
+        nome = request.POST.get('nome', '')
+        idade = request.POST.get('idade', 0)
+        pessoa = get_object_or_404(Pessoa, id=id)
+        pessoa.nome = nome or 'Sem nome'
+        pessoa.idade = idade
+        pessoa.save()
+    return redirect('home')
+
+def delete(request, id):
+    pessoa = get_object_or_404(Pessoa, id=id)
+    pessoa.delete()
+    return redirect('home')
