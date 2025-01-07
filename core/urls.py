ba709cb26e7676612f2cd1b66a488b22afbd8f27
mauro <mauro_leao@hotmail.com>
2025-01-07 17:51:19 -0300

    implementado a model da aplicação core e feito as migrações no banco

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
+from .views import home
 
 urlpatterns = [
-    path('core/', include('core.urls')),
+    path('', home),
     
 ]
